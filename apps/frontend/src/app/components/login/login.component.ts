31a99d0b854bd9df2bb6d26537eb84a9e729117c
Aayush <Aayush.Aggarwal@gds.ey.com>
2025-08-04 18:48:28 +0530

    Code refactor

--- apps/frontend/src/app/components/login/login.component.ts
+++ apps/frontend/src/app/components/login/login.component.ts
@@ -4,23 +4,22 @@ import { UserService } from 'src/app/services/user.service';
 import { User } from '../../models';
 import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInput } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule],
+  imports: [FormsModule, MatButtonModule, MatInput, MatFormFieldModule],
   template: `
     <div class="login-container">
       <form class="login-form" (submit)="login()">
         <h2>Sign In</h2>
-        <input
-          type="text"
-          placeholder="Username"
-          [(ngModel)]="username"
-          name="username"
-          required
-          autocomplete="off"
-        />
-        <button type="submit">Login</button>
+        <mat-form-field appearance="fill">
+          <mat-label>Username</mat-label>
+          <input type="text" matInput [(ngModel)]="username" required autocomplete="off" name="username">
+        </mat-form-field>
+        <button mat-button type="submit" [disabled]="!username">Login</button>
       </form>
     </div>
   `,
